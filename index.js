be8822481310ef6557005b3c71ae9096794314a2
Abiodun Oyebanji <stepheng323@gmail.com>
2019-10-15 03:48:19 +0100

    touch up the ui element

---
+++ index.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger');
+const card = document.querySelector('.card');
+
+const openSlideMenu = () => {
+  document.getElementById('side-menu').style.width = '250px';
+  document.querySelector('.main-container').style.marginRight = '250px';
+  hamburger.style.display = 'none';
+};
+
+const closeSlideMenu = () => {
+  document.getElementById('side-menu').style.width = '0';
+  document.querySelector('.main-container').style.marginRight = '0';
+  hamburger.style.display = 'unset';
+};
+
+const seeProduce = () => {
+  card.scrollIntoView({
+    behavior: 'smooth'
+  });
+};
